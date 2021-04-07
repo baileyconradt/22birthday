e1ad88b4d17faa947136226be8ef12bca4004c7b
Bailey <baileyconradt@gmail.com>
2021-04-06 22:37:39 -0500

    initial app is up!

---
+++ 22birthday-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a target="_blank" href='https://baileyconradt.com'><img src='https://media.giphy.com/media/xT1R9G7vPViOVd0faE/giphy.gif' /></a>
+        <p>
+         Shake it off!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
